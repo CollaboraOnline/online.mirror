1a9bac8d6efeb240316e79ee051ac5b7141bf248
Gülşah Köse <gulsah.kose@collabora.com>
2026-04-14 08:42:12 +0300

    Fix other cursors are displayed unrelated slide
    The _onViewCursorVisibleMsg handler was only checking whether
    the cursor is meant to be visible - it doesn't check whether the
    other user is on the same slide/part.
    
    The new control returns false when the cursor's part differs from
    the local user's selected part.
    
    Also hide the other users cursor tooltips immediately without waiting
    10ms timeout.
    
    Signed-off-by: Gülşah Köse <gulsah.kose@collabora.com>
    Change-Id: I09f1e0116284b85c34e9a93686f0d398cec895fc
    (cherry picked from commit b4760eae38a0ba94a6990008feb745a2db3a40e1)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/3214
    Reviewed-by: Gülşah Köse <gulsahkose@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -1956,7 +1956,9 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 		if (section) {
 			const showCursor = obj.visible === 'true';
 			section.sectionProperties.showCursor = showCursor;
-			section.setShowSection(showCursor);
+			section.setShowSection(section.checkMyVisibility());
+			if (!section.showSection)
+				CursorHeaderSection.deletePopUpNow(viewId);
 		}
 	},
 
