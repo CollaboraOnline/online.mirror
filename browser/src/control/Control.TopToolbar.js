f7b964531114cc22f789d9da9ebaa07c051e074c
Jaume Pujantell <jaume.pujantell@collabora.com>
2026-06-05 11:21:26 +0000

    send programmatic style name to core
    The compact toolbar styles combobox displays localized labels via
    styleMappings[name].toLocaleString(), so picking "Heading 1" in
    German sends "Überschrift 1" back to core. Core's .uno:StyleApply
    expects the programmatic name, and the apply can silently no-op
    when it doesn't get one.
    
    Stash the programmatic name of each entry in an array at build time and
    look it up on select. Simply reverse-translating the label through
    styleMappings might be ambiguous when multiple keys share a localized
    form (for example "textbody" and "Text body" both map to "Body Text").
    
    This make the engine fix in c589b3e4c5e892db648744a77823e2aa9c9baec
    "sw: apply paragraph style sent with localized UI name" unnecessary, but
    it is left as a fallback.
    
    Change-Id: Ia1be8d609f0358f5cd8edcf243a98c3f3d040115
    Signed-off-by: Jaume Pujantell <jaume.pujantell@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/4227
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 729c4c6b6e48a28f6e81e41e4ce28ea3e715b1b3)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/4636

--- browser/src/control/Control.TopToolbar.js
+++ browser/src/control/Control.TopToolbar.js
@@ -79,9 +79,14 @@ class TopToolbar extends JSDialog.Toolbar {
 			return;
 		}
 		if (object.id === 'styles') {
-			if (eventType === 'selected')
-				this.onStyleSelect({target: {value: data.substr(data.indexOf(';') + 1)}});
-			else if (eventType === 'change')
+			if (eventType === 'selected') {
+				const pos = parseInt(data.substr(0, data.indexOf(';')), 10);
+				const label = data.substr(data.indexOf(';') + 1);
+				const style = (this.styleProgNames && this.styleProgNames[pos] !== undefined)
+					? this.styleProgNames[pos]
+					: label;
+				this.onStyleSelect({target: {value: style}});
+			} else if (eventType === 'change')
 				this.onStyleSelect({target: {value: data}});
 			return;
 		}
@@ -482,6 +487,7 @@ class TopToolbar extends JSDialog.Toolbar {
 		}
 
 		var entries = [];
+		this.styleProgNames = [];
 		var commands = commandValues.Commands;
 		if (commands && commands.length > 0) {
 			commands.forEach(function (command) {
@@ -490,7 +496,8 @@ class TopToolbar extends JSDialog.Toolbar {
 					translated = window.L.Styles.styleMappings[command.text].toLocaleString();
 				}
 				entries.push(translated);
-			});
+				this.styleProgNames.push(command.id);
+			}, this);
 		}
 
 		var styles = [];
@@ -505,7 +512,8 @@ class TopToolbar extends JSDialog.Toolbar {
 
 		topStyles.forEach(function (style) {
 			entries.push(window.L.Styles.styleMappings[style].toLocaleString());
-		});
+			this.styleProgNames.push(style);
+		}, this);
 
 		if (styles !== undefined && styles.length > 0) {
 			styles.forEach(function (style) {
@@ -518,7 +526,8 @@ class TopToolbar extends JSDialog.Toolbar {
 					localeStyle = localeStyle === undefined ? style : localeStyle.toLocaleString();
 				}
 				entries.push(localeStyle);
-			});
+				this.styleProgNames.push(style);
+			}, this);
 		}
 
 		var container = document.getElementById('styles');
