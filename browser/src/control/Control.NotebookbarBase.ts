ee19c93a16e2e17676b23ee48b21233c66814662
Samuel Mehrbrodt <samuel.mehrbrodt@collabora.com>
2026-07-09 10:52:21 +0200

    cool#15537 Restore focus to the document after choosing a combobox entry
    Picking a style from the sidebar's paragraph style box left focus
    there instead of the document. Reuse the dialog check added for
    iconviews, and fix the other panels with the same gap.
    
    Signed-off-by: Samuel Mehrbrodt <samuel.mehrbrodt@collabora.com>
    Change-Id: I8e3eb4debabd89e8b5fc6864f845a3e493cedee3
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6565
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    (cherry picked from commit b7955f549d6dd55fa42ad76c98fa71685183b4b0)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/8687

--- browser/src/control/Control.NotebookbarBase.ts
+++ browser/src/control/Control.NotebookbarBase.ts
@@ -112,7 +112,7 @@ class NotebookbarBase extends JSDialogComponent {
 			);
 			if (consumed) return;
 		}
-		this.builder?._defaultCallbackHandler(
+		return this.builder?._defaultCallbackHandler(
 			objectType,
 			eventType,
 			object,
