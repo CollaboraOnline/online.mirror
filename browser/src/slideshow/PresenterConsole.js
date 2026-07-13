73de4a525c9b1cfc0108c2bfb86b06383bfc756e
shlok3640 <shlok.kanani@collabora.com>
2026-07-13 09:02:21 -0400

    slideshow: prevent text cropping in presenter console thumbnail
    The 'Click to exit presentation' text in the presenter console's next
    slide thumbnail would get cropped if the thumbnail was too narrow.
    
    Passing 'width - 20' as the maxWidth to fillText ensures the HTML5
    canvas automatically scales down the text to fit perfectly within the
    container.
    
    Signed-off-by: shlok3640 <shlok.kanani@collabora.com>
    Change-Id: I86322d9b1836658bfe19ee07e552fa538232415c
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/7835
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -1374,9 +1374,15 @@ class PresenterConsole {
 				_('Repeat in {0} seconds').replace('{0}', repeat),
 				width / 2,
 				height / 2,
+				Math.max(0, width - 20),
 			);
 		} else {
-			ctx.fillText(_('Click to exit presentation...'), width / 2, height / 2);
+			ctx.fillText(
+				_('Click to exit presentation...'),
+				width / 2,
+				height / 2,
+				Math.max(0, width - 20),
+			);
 		}
 
 		return offscreen.convertToBlob({ type: 'image/png' });
