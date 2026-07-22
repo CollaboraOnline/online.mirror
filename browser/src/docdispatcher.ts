LibreOffice/core

CODA: aichat: fix the AI provider setup flow on the desktop app
On the desktop app the File menu opens the backstage over the document, so telling an unconfigured user to go to File > Options
leads them into the backstage, and after saving the AI settings the backstage still covers the document and the new AI sidebar.

- An AI action with no provider configured now opens the Options dialog directly over the document, scrolled to the AI section. The snackbar with the menu path stays for the browser, where File >  Options opens the same dialog without the backstage.
- After a save that configures a provider, the backstage closes before the View tab and the AI sidebar open.
- Opening the View tab skips the click on the tab label when that tab is already selected and the bar is expanded, because that click collapses the notebookbar.
- The apps treat a provider without an API key as not configured. The native model-list fetch authenticates with the key, so a provider without one cannot complete setup there; on a server the key stays optional for self-hosted endpoints. Saving without a key points at the missing key instead of opening the sidebar, and removing the key closes an open sidebar.

Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
Change-Id: Iccb060f9e27504b77757d85267e199800df669d8
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6566
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Rashesh Padia <rashesh.padia@collabora.com>

browser/src/docdispatcher.ts
@@ -570,14 +570,28 @@ class Dispatcher {
 		}
 	}
 
+	// An AI action was triggered before a provider was configured. On the
+	// desktop app the Options dialog opens right here, over the document,
+	// scrolled to the AI section, so the user sets the provider up in place
+	// and lands back on the document when they save. In a browser the File
+	// menu leads straight to the same dialog, so a snackbar with the path
+	// is enough.
+	private static promptAISetup() {
+		if (window.mode.isCODesktop()) {
+			(app.map as any).settings.showSettingsDialog('ai-section');
+		} else {
+			app.map.uiManager.showSnackbar(
+				_(
+					'AI is not configured. Go to File > Options > View Settings to set it up.',
+				),
+			);
+		}
+	}
+
 	private addAICommands() {
 		this.actionsMap['aichat'] = function () {
 			if (!app.map.isAIConfigured) {
-				app.map.uiManager.showSnackbar(
-					_(
-						'AI is not configured. Go to File > Options > View Settings to set it up.',
-					),
-				);
+				Dispatcher.promptAISetup();
 				return;
 			}
 			const sidebar = JSDialog.getAIChatSidebar();
@@ -586,11 +600,7 @@ class Dispatcher {
 
 		this.actionsMap['helpfixformulaerror'] = function () {
 			if (!app.map.isAIConfigured) {
-				app.map.uiManager.showSnackbar(
-					_(
-						'AI is not configured. Go to File > Options > View Settings to set it up.',
-					),
-				);
+				Dispatcher.promptAISetup();
 				return;
 			}
 			const sidebar = JSDialog.getAIChatSidebar();

CollaboraOnline/online.mirror

(cherry picked from commit ac6ca8a3aa33971561555f2cdfea006635c4d75c)
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6836

browser/src/docdispatcher.ts
@@ -390,14 +390,28 @@ class Dispatcher {
 		};
 	}
 
+	// An AI action was triggered before a provider was configured. On the
+	// desktop app the Options dialog opens right here, over the document,
+	// scrolled to the AI section, so the user sets the provider up in place
+	// and lands back on the document when they save. In a browser the File
+	// menu leads straight to the same dialog, so a snackbar with the path
+	// is enough.
+	private static promptAISetup() {
+		if (window.mode.isCODesktop()) {
+			(app.map as any).settings.showSettingsDialog('ai-section');
+		} else {
+			app.map.uiManager.showSnackbar(
+				_(
+					'AI is not configured. Go to File > Options > AI Assistant to set it up.',
+				),
+			);
+		}
+	}
+
 	private addAICommands() {
 		this.actionsMap['aichat'] = function () {
 			if (!app.map.isAIConfigured) {
-				app.map.uiManager.showSnackbar(
-					_(
-						'AI is not configured. Go to File > Options > AI Assistant to set it up.',
-					),
-				);
+				Dispatcher.promptAISetup();
 				return;
 			}
 			const sidebar = JSDialog.getAIChatSidebar();
@@ -406,11 +420,7 @@ class Dispatcher {
 
 		this.actionsMap['helpfixformulaerror'] = function () {
 			if (!app.map.isAIConfigured) {
-				app.map.uiManager.showSnackbar(
-					_(
-						'AI is not configured. Go to File > Options > AI Assistant to set it up.',
-					),
-				);
+				Dispatcher.promptAISetup();
 				return;
 			}
 			const sidebar = JSDialog.getAIChatSidebar();