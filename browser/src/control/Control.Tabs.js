6106305b1dd260f9a52a5042aa519d09a2137978
Mohit Marathe <mohit.marathe@collabora.com>
2026-07-30 08:42:47 +0000

    browser: no sheet-rename dialog on a read-only sheet tab
    The tab context menu and the drag handlers already check
    isReadOnlyMode. The double-click handler did not, so it opened the
    client-side rename dialog.
    
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I54b7f628c537176790c1f90945d920766b556c84
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/8266
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/8613
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- browser/src/control/Control.Tabs.js
+++ browser/src/control/Control.Tabs.js
@@ -247,6 +247,8 @@ window.L.Control.Tabs = window.L.Control.extend({
 						window.L.DomEvent.on(tab, 'dblclick', function(j) {
 							return function() {
 								// window.app.console.err('Double clicked ' + j);
+								if (this._map.isReadOnlyMode())
+									return;
 								this._tabForContextMenu = j;
 								this._renameSheet();
 							};
