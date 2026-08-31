f4a57d93815060f8b9ee14020b19af5419bd10ab
Szymon Kłos <szymon.klos@collabora.com>
2026-08-31 10:51:07 +0000

    jsdialog: restore toolbar dynamic entries after modification
    - it can happen that integration puts new items into toolbar
    - do not drop dynamically received enries but reload them from
      model on rebuild
    
    Solved conflict, compared to 26.04:
    - widgetUpdate has different parameter expected
    - types don't match with 26.04, used cast to any
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I8155446913fbb2d003070f810964a9ac2f80e5c9
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/10563
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Tomaž Vajngerl <tomaz.vajngerl@collabora.com>
    (cherry picked from commit f4598f089b91cac01a230cdd1eb8a790e8b3b583)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/10842

--- browser/src/control/Control.TopToolbar.js
+++ browser/src/control/Control.TopToolbar.js
@@ -318,7 +318,18 @@ class TopToolbar extends JSDialog.Toolbar {
 		this.reset();
 
 		var items = this.getToolItems();
-		this.builder.build(this.parentContainer, items);
+
+		var json = {
+			id: this.toolbarElementId,
+			dialogid: this.toolbarElementId,
+			jsontype: 'toolbar',
+			type: 'toolbox',
+			children: items,
+		};
+		var previousModel = this.model.getSnapshot();
+		this.model.fullUpdate(json);
+		this.restoreDynamicEntries(previousModel);
+		this.builder.build(this.parentContainer, this.model.getSnapshot().children);
 
 		if (window.mode.isMobile()) {
 			JSDialog.MakeScrollable(this.parentContainer, this.parentContainer.querySelector('div'));
@@ -392,14 +403,11 @@ class TopToolbar extends JSDialog.Toolbar {
 		case 'presentation':
 			{
 				// Fill the style combobox with Impress layouts
-				var stylesContainer = document.getElementById('styles');
-				if (stylesContainer && stylesContainer.updateEntries) {
-					var layoutEntries = [];
-					window.L.Styles.impressLayout.forEach(function(layout) {
-						layoutEntries.push(_(layout.text));
-					});
-					stylesContainer.updateEntries(layoutEntries);
-				}
+				var layoutEntries = [];
+				window.L.Styles.impressLayout.forEach(function(layout) {
+					layoutEntries.push(_(layout.text));
+				});
+				this.updateComboboxEntries('styles', layoutEntries);
 			}
 
 			if (this.parentContainer) {
@@ -530,12 +538,11 @@ class TopToolbar extends JSDialog.Toolbar {
 			}, this);
 		}
 
+		this.updateComboboxEntries('styles', entries);
+
 		var container = document.getElementById('styles');
-		if (container && container.updateEntries) {
-			container.updateEntries(entries);
-			if (this.stylesSelectValue)
-				container.onSetText(this.stylesSelectValue);
-		}
+		if (container && this.stylesSelectValue)
+			container.onSetText(this.stylesSelectValue);
 	}
 
 	processStateChangedCommand(commandName, state) {
