15a4807232654740e55f33d76bfcc02aa7a3c998
Miklos Vajna <vmiklos@collabora.com>
2026-09-02 13:34:31 +0200

    cool#16131 engine SVG paste: handle this in Writer
    If a clipboard with mime type image/svg+xml reaches the kit process
    successfully, nothing appears in the Writer document.
    
    Three layers of the paste path drop the SVG on the floor. The sot/
    tables listed PNG/JPEG/etc but not SVG. Then vcl/ had no code to
    actually import SVG from the clipboard. Finally in sw/, SwTransferable
    looked for PNG/JPEG/etc, but not SVG.
    
    Add an SVG branch to vcl's GetGraphic() that pulls the sot storage
    stream and runs it through GraphicConverter::Import(...,
    ConvertDataFormat::SVG). Add SVG rows to the svl/ tables.  Add SVG
    cases to the paste-dispatch switches in SwTransferable. With this, once
    the clipboard contains an SVG image, it gets pasted into Writer at
    least.
    
    The order of the entries in the sot/ tables matter. SVG has to be before
    GDI, because 6c04b8356c0764ba8ac51448eee5ff577b08d9e2 (i#112829: offer
    svg as clipboard format as well, 2024-02-15) advertises GDI when we have
    SVG, but the kit clipboard (as a "source application to paste from") has
    no code to convert an SVG to GDIMetaFile.
    
    (cherry picked from commit 22962f79023bcafa3510e9a695585f126ed63b42)
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I444a317364a24bca5e5bf876c856bd587df1b7d3
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/11680
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- engine/sw/source/uibase/dochdl/swdtflvr.cxx
+++ engine/sw/source/uibase/dochdl/swdtflvr.cxx
@@ -1937,6 +1937,7 @@ bool SwTransferable::PasteData( const TransferableDataHelper& rData,
             case SotClipboardFormatId::BITMAP:
             case SotClipboardFormatId::PNG:
             case SotClipboardFormatId::GDIMETAFILE:
+            case SotClipboardFormatId::SVG:
                 bRet = SwTransferable::PasteGrf( rData, rSh, nFormat,
                                                 SwPasteSdr::Insert,pPt,
                         nActionFlags, nDropAction, bNeedToSelectBeforePaste,
@@ -2059,6 +2060,7 @@ bool SwTransferable::PasteData( const TransferableDataHelper& rData,
             case SotClipboardFormatId::GDIMETAFILE:
             case SotClipboardFormatId::BITMAP:
             case SotClipboardFormatId::PNG:
+            case SotClipboardFormatId::SVG:
             case SotClipboardFormatId::NETSCAPE_BOOKMARK:
             case SotClipboardFormatId::SIMPLE_FILE:
             case SotClipboardFormatId::FILEGRPDESCRIPTOR:
@@ -2916,6 +2918,7 @@ bool SwTransferable::PasteGrf( const TransferableDataHelper& rData, SwWrtShell& 
     case SotClipboardFormatId::BITMAP:
     case SotClipboardFormatId::PNG:
     case SotClipboardFormatId::GDIMETAFILE:
+    case SotClipboardFormatId::SVG:
         bRet = rData.GetGraphic( nFormat, aGraphic );
         break;
 
