9f140b1557738810222d835cfcd232761761cbb7
tigran54 <tigran@flib.sci.am>
2026-09-09 07:51:04 +0200

    Translated using Weblate (Armenian)
    Currently translated at 100.0% (41 of 41 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/hy/
    Signed-off-by: tigran54 <tigran@flib.sci.am>
    Change-Id: I8da287f78ac86400b70320fd2f230bc3569e6367

--- qt/translations/coda_hy.ts
+++ qt/translations/coda_hy.ts
@@ -152,22 +152,22 @@
     <message>
         <location filename="../Bridge.cpp" line="353"/>
         <source>Export Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Արտահանման սխալ</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="354"/>
         <source>Failed to export the document.</source>
-        <translation type="unfinished"></translation>
+        <translation>Փաստաթուղթը չհաջողվեց արտահանել։</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="939"/>
         <source>Cannot open remote document</source>
-        <translation type="unfinished"></translation>
+        <translation>Հնարավոր չէ բացել հեռավոր փաստաթուղթը</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="942"/>
         <source>the Collabora Online server</source>
-        <translation type="unfinished"></translation>
+        <translation>Collabora Online կայան</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="1129"/>
@@ -199,12 +199,12 @@
     <message>
         <location filename="../DBusService.cpp" line="86"/>
         <source>Template Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ձևանմուշի սխալ</translation>
     </message>
     <message>
         <location filename="../DBusService.cpp" line="87"/>
         <source>Could not create a new document from this template.</source>
-        <translation type="unfinished"></translation>
+        <translation>Այս ձևանմուշից նոր փաստաթուղթ ստեղծել հնարավոր չէ։</translation>
     </message>
 </context>
 <context>
