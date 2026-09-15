LibreOffice/core

Avoid sending potentially confusing Cache-Control header on cool.html
We never want to cache this file, and it is generated from a POST,
so send an explicit no-store instead.

Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
Change-Id: I983e489396ecee144daeb5b0369ebd56a0738b15
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/8279
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Hubert Figuière <hub@collabora.com>

wsd/FileServer.cpp
@@ -581,7 +581,8 @@ bool FileServerRequestHandler::handleRequest(const HTTPRequest& request,
             endPoint == "localizations.json" ||
             endPoint == "uno-localizations.json")
         {
-            accessDetails = preprocessFile(request, response, requestDetails, message, socket);
+            accessDetails = preprocessFile(request, response, requestDetails, message,
+                                           endPoint == "cool.html" /* noCache */, socket);
             return true;
         }
 
@@ -1448,7 +1449,7 @@ void FileServerRequestHandler::replaceServiceRoot(const HTTPRequest& request,
 
 FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::preprocessFile(
     const HTTPRequest& request, http::Response& httpResponse, const RequestDetails& requestDetails,
-    std::istream& message, const std::shared_ptr<StreamSocket>& socket)
+    std::istream& message, bool noCache, const std::shared_ptr<StreamSocket>& socket)
 {
     const ServerURL cnxDetails(requestDetails);
 
@@ -1768,8 +1769,15 @@ FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::prepro
     }
 
     httpResponse.set("Last-Modified", Util::getHttpTimeNow());
-    httpResponse.set("Cache-Control", "max-age=11059200");
-    httpResponse.set("ETag", COOLWSD_VERSION_HASH);
+    if (noCache) // critical to serve cool.html each time
+    {
+        httpResponse.set("Cache-Control", "no-store");
+    }
+    else
+    {
+        httpResponse.set("Cache-Control", "max-age=11059200");
+        httpResponse.set("ETag", COOLWSD_VERSION_HASH);
+    }
 
     httpResponse.add("X-Content-Type-Options", "nosniff");
     httpResponse.add("X-XSS-Protection", "1; mode=block");

CollaboraOnline/online.mirror

Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/8373
(cherry picked from commit eb3fc392719554007d814b583e45e7ad31df463a)
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/9232

wsd/FileServer.cpp
@@ -1037,7 +1037,8 @@ bool FileServerRequestHandler::handleRequest(const HTTPRequest& request,
             endPoint == "uno-localizations.json" ||
             endPoint == "uno-localizations-override.json")
         {
-            accessDetails = preprocessFile(request, response, requestDetails, message, socket);
+            accessDetails = preprocessFile(request, response, requestDetails, message,
+                                           endPoint == "cool.html" /* noCache */, socket);
             return true;
         }
 
@@ -1701,7 +1702,7 @@ std::string boolToString(const bool value)
 
 FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::preprocessFile(
     const HTTPRequest& request, http::Response& httpResponse, const RequestDetails& requestDetails,
-    Poco::MemoryInputStream& message, const std::shared_ptr<StreamSocket>& socket)
+    Poco::MemoryInputStream& message, bool noCache, const std::shared_ptr<StreamSocket>& socket)
 {
     const ServerURL cnxDetails(requestDetails);
 
@@ -2006,8 +2007,15 @@ FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::prepro
     }
 
     httpResponse.set("Last-Modified", Util::getHttpTimeNow());
-    httpResponse.set("Cache-Control", "max-age=11059200");
-    httpResponse.set("ETag", COOLWSD_VERSION_HASH);
+    if (noCache) // critical to serve cool.html each time
+    {
+        httpResponse.set("Cache-Control", "no-store");
+    }
+    else
+    {
+        httpResponse.set("Cache-Control", "max-age=11059200");
+        httpResponse.set("ETag", COOLWSD_VERSION_HASH);
+    }
 
     httpResponse.add("X-Content-Type-Options", "nosniff");
     httpResponse.add("X-XSS-Protection", "1; mode=block");