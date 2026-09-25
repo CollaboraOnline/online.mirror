d7f348037bcca9ae4d46599fc7f65bd48e2846dd
Stephan Bergmann <stephan.bergmann@collabora.com>
2026-09-25 10:25:25 +0000

    Drop stale com.sun.star.deployment.test module export
    ...after 676eedfdf676855314958ca7244ab94fff91b7a4 "smoketest: drop the module"
    removed the only IDL in that package
    
    Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Change-Id: I9fea71ddf8b8ba1e6eef57a669a1c70d797e6c56
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/13536
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- engine/ridljar/source/libreoffice/module-info.java
+++ engine/ridljar/source/libreoffice/module-info.java
@@ -38,7 +38,6 @@ module org.libreoffice.uno
     exports com.sun.star.datatransfer.clipboard;
     exports com.sun.star.datatransfer.dnd;
     exports com.sun.star.deployment;
-    exports com.sun.star.deployment.test;
     exports com.sun.star.document;
     exports com.sun.star.drawing;
     exports com.sun.star.drawing.framework;
