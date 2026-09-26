873b72f02685ba512630472695bfff6b3bc7a5d8
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-09-26 01:38:05 +0000

    wait for the click timer to expire inside removeShapeSelection
    mobile/impress/apply_font_text_spec.js failed with:
    
        Timed out retrying after 60000ms: Expected to find element: `#test-div-shape-handle-2`, but never found it.
    
    The next click after removeShapeSelection could land within the 250 ms
    click timer and go out as a double click. Only
    triggerNewSVGForShapeInTheCenter waited for the timer, so move the
    wait into removeShapeSelection itself.
    
    Also look #CharBackColor up again after scrollIntoView, because a sidebar
    update can replace the header in between.
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ib6700924cbffed97497ad1f5b9ae209570db2911
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/13596
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- cypress_test/integration_tests/common/impress_helper.js
+++ cypress_test/integration_tests/common/impress_helper.js
@@ -131,11 +131,14 @@ function removeShapeSelection() {
 			cy.cGet('body').click(XPos, YPos);
 		});
 
+	// A click within 250 ms of this one is sent as a double click, wherever it lands.
+	//
 	// The click triggers MouseControl.onClick which calls focus(false),
 	// blurring the textarea on mobile. Re-focus depends on INCOMING
 	// invalidatecursor from core. processToIdle ensures that message
 	// has been received before we send the Esc keys.
 	cy.getFrameWindow().then(function(win) {
+		helper.waitForTimers(win, 'clicktimer');
 		helper.processToIdle(win);
 	});
 
@@ -170,10 +173,6 @@ function triggerNewSVGForShapeInTheCenter() {
 	cy.log('>> triggerNewSVGForShapeInTheCenter - start');
 
 	removeShapeSelection();
-
-	cy.getFrameWindow().then(function(win) {
-		helper.waitForTimers(win, 'clicktimer');
-	});
 
 	// Select text shape again which will retrigger a new SVG from core
 	selectTextShapeInTheCenter();
